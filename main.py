5746fd4f4c0fb3b0e1f34bb88d13807ffc94796b
jeanjerome <jeanjerome.levy@gmail.com>
2024-03-31 17:16:51 +0200

    Update to crewai 0.22.4 + Simplify backstory and description.

--- main.py
+++ main.py
@@ -2,100 +2,73 @@ from crewai import Agent, Task, Process, Crew
 from langchain_community.llms import Ollama
 
 # To Load Local models through Ollama
-llm_mixtral = Ollama(model="mixtral")
-#llm_coder = Ollama(model="codellama:34b")
+llm_model = Ollama(model="mixtral")
+coding_model = Ollama(model="codellama:34b")
 
 po = Agent(
     role="Product Owner",
     goal="Ensure the detailed drafting of user stories",
     backstory="""As the Product Owner of an Agile team, you excel at comprehending market demands, identifying the target audience, 
-        and analyzing the competition. This expertise is essential for validating if a concept addresses a market need and possesses 
-        the potential to captivate a broad audience. You are skilled at devising strategies to appeal to the widest possible audience, 
+        and analyzing the competition. You are skilled at devising strategies to appeal to the widest possible audience, 
         ensuring the product aligns with user stories and meets market expectations.
-		""",
+	""",
     verbose=True,
     allow_delegation=False,
-    llm=llm_mixtral
+    llm=llm_model
 )
 
 developer = Agent(
-role="Bash Scripting Expert",
-goal="Implement the requirements outlined in each user story through coding",
-backstory="""You are a master of Bash scripting, with a profound knowledge of Unix-based systems. Your expertise is in writing 
-        scripts and also understanding how these scripts can streamline operations, automate mundane tasks, and solve complex technical 
-        challenges. With a keen eye for detail and a deep understanding of system architecture, you adeptly craft scripts that enhance 
-        productivity and ensure robust system performance. Your ability to decipher and optimize existing scripts, as well as to innovate 
-        new solutions, makes you an invaluable asset. Your insights and contributions are key in optimizing workflows, improving operational 
-        reliability, and driving technological efficiency.""",
+    role="Bash Scripting Expert",
+    goal="Implement the requirements outlined in each user story through coding",
+    backstory="""You are a master of Bash scripting, with a profound knowledge of Unix-based systems.""",
     verbose=True,
     allow_delegation=False,
-    llm=llm_mixtral
+    llm=coding_model
 )
 
 reviewver = Agent(
-role="Reviewer",
-goal="Review the code to assess the quality, maintainability, and alignment with the team's standards and best practices",
-backstory="""You are a guardian of code quality, with a keen understanding of Agile development practices and a sharp eye for detail in code review. 
-        Your expertise goes beyond mere code inspection; you are adept at ensuring that developments not only function as intended but also adhere 
-        to the team's coding standards, enhance maintainability, and seamlessly integrate with existing systems. With a deep appreciation for 
-        collaborative development, you provide constructive feedback, guiding contributors towards best practices and fostering a culture of 
-        continuous improvement. Your meticulous approach to reviewing code, coupled with your ability to foresee potential issues and recommend 
-        proactive solutions, ensures the delivery of high-quality software that is robust, scalable, and aligned with the team's strategic goals.""",
+    role="Reviewer",
+    goal="Review the code to assess the quality, maintainability, and alignment with state-of-the-art and best practices",
+    backstory="""You are a guardian of code quality, with a sharp eye for detail in code review. You are adept at ensuring 
+        that developments not only function as intended but also adhere to state-of-the-art-standards. With a deep 
+        appreciation for collaborative development, you provide constructive feedback, guiding contributors towards 
+        best practices and fostering a culture of continuous improvement.
+    """,
     verbose=True,
-    allow_delegation=True,
-    llm=llm_mixtral
+    allow_delegation=False,
+    llm=llm_model
 )
 
 task1 = Task(
     description="""Develop user stories for a Bash script wrapper function designed to :
         - Execute commands with parameters,
         - Log execution information to a file, 
-        - Manage status codes,
+        - Manage errors by logging detailed stack trace,
         - Output result to stdout when available.
-        This tool aims to streamline and automate processes, enhancing operational efficiency and reliability. Your user stories should 
+        This tool aims to enhance operational efficiency and reliability. Your user stories should 
         clearly articulate the needs and expectations of the users, focusing on how they will interact with the wrapper to perform tasks 
         more effectively. Include scenarios covering a range of use cases, from simple command execution to complex workflows involving 
         error handling and output management. Ensure that each user story is detailed, specifying the context, the user's goal, and the
         desired outcome, to guide the development team in creating a solution that meets users' needs.
-        These keywords must never be translated and transformed:
-        - Action:
-        - Thought:
-        - Action Input:
-        because they are part of the thinking process instead of the output.
-        Action Input should be formatted as coworker|task|context.
     """,
+    expected_output="a title and a definition of done",
     agent=po,
 )
 
 task2 = Task(
-    description="""Implement the user stories developed by your Product Owner. Your implementation should thoroughly 
-    address each user story's requirements, providing a seamless experience for the end-users, focusing on creating a robust and efficient tool. 
-    The task involves coding the various operational scenarios described in the provided user stories. You will follow 'the stdout is for output, 
-    stderr is for messaging' principal. You ensure your code is clean, well-documented, and adheres to best practices for script development. 
-    The final product should be a code formatted in markdown.
-    These keywords must never be translated and transformed:
-    - Action:
-    - Thought:
-    - Action Input:
-    because they are part of the thinking process instead of the output.
-    Action Input should be formatted as coworker|task|context.
+    description="""Using the user stories provided, develop a robust and efficient tool. Your code should follow the stdout is for output, 
+        the stderr is for messaging principal. You ensure your code is clean, simple, and adheres to best practices for shell script development.
     """,
+    expected_output="markdown",
     agent=developer,
 )
 
 task3 = Task(
-    description="""Ensure the delivery is a code based. Ensure the quality of the code, the adherence to coding standards, and the respect 
-    of the specifications included in the user stories. Provide detailed feedback to developers, highlighting areas for improvement, potential 
-    bugs, and suggestions for optimization. Your review should include a checklist of criteria that align with best practices in software 
-    development and the specific requirements of the project. Ensure that the code is not only functional but also maintainable and scalable. 
-    Collaborate with the development team to achieve high-quality software delivery in the project.
-    These keywords must never be translated and transformed:
-    - Action:
-    - Thought:
-    - Action Input:
-    because they are part of the thinking process instead of the output.
-    Action Input should be formatted as coworker|task|context.
+    description="""Ensure the quality of the code, the adherence to the SOLID principals, and the respect 
+        of the specifications included in the user stories. Provide detailed feedback to developers, highlighting areas for improvement, potential 
+        bugs, and suggestions for optimization. Collaborate with the development team to achieve high-quality software delivery in the project.
     """,
+    expected_output="Full report in bullet points",
     agent=reviewver,
 )
 
